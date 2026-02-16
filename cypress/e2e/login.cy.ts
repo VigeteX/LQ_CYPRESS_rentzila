c44ea3de1d1517002e2271f20062880a985fbddb
VigeteX <73284922+VigeteX@users.noreply.github.com>
2026-02-16 17:51:37 +0200

    C212 C213 C214 C530(p1, p2, p3)

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -9,10 +9,11 @@ describe('Login flow', () => {
     beforeEach(() => {
         cy.viewport(1920, 1080);
         cy.visit('/');
-        header.elements.enterButton().click();
     });
 
     it('C200 Authorization with empty field', () => {
+        header.elements.enterButton().click();
+
         loginPage.elements.emailField().type(validUser.email).should('have.value', validUser.email);
         loginPage.elements.submitButton().click();
         loginPage.check_error_message(errorMessages.emptyField)
@@ -25,6 +26,8 @@ describe('Login flow', () => {
     });
    
     it('C201 Authorization with valid email and password', () => {
+        header.elements.enterButton().click();
+
         loginPage.elements.emailField().type(validUser.email).should('have.value', validUser.email);
         loginPage.elements.passwordField().type(validUser.password).should('have.value', validUser.password);;
 
@@ -44,6 +47,7 @@ describe('Login flow', () => {
     describe('C202 Authorization with valid phone and password', () => {
         validPhones.forEach(phone => {
             it(`valid phone: ${phone}`, () => {
+                header.elements.enterButton().click();
                 loginPage.login(phone, validUser.password)
 
                 header.elements.avatarIcon().click();
@@ -56,6 +60,7 @@ describe('Login flow', () => {
     describe('C203 Authorization with invalid credentials', () => {
         invalidEmails.forEach(email  => {
             it(`invalid email: ${email}`, () => {
+                header.elements.enterButton().click();
                 loginPage.login(email, validUser.password)
                 loginPage.check_error_message(errorMessages.invalidEmailOrPhone)
                 loginPage.elements.authClose().click()
@@ -64,12 +69,14 @@ describe('Login flow', () => {
 
         invalidPasswords.forEach(password  => {
             it(`invalid password: ${password}`, () => {
+                header.elements.enterButton().click();
                 loginPage.login(validUser.email, password)
                 loginPage.check_error_message(errorMessages.invalidPassword)
                 loginPage.elements.authClose().click()
             });
         });
         it('wrong password with valid format', () => {
+            header.elements.enterButton().click();
             loginPage.login(validUser.email,wrongPassword)
             loginPage.elements.errorMessage().should('be.visible').and('have.text', 'Невірний e-mail або пароль')
             loginPage.elements.authClose().click()
@@ -79,6 +86,7 @@ describe('Login flow', () => {
     describe('C207 Authorization with invalid phone', () => {
         invalidPhones.forEach(phone => {
             it(`invalid phone: ${phone}`, () => {
+                header.elements.enterButton().click();
                 loginPage.login(phone, validUser.password)
                 loginPage.check_error_message(errorMessages.invalidEmailOrPhone)
                 loginPage.elements.authClose().click()
