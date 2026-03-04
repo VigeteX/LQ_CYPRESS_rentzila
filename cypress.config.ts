16aaccfb2a030f25e2ce30957a2692a4df665d3f
VigeteX <73284922+VigeteX@users.noreply.github.com>
2026-03-04 22:03:33 +0200

    -

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,34 @@
-import { defineConfig } from 'cypress';
-import 'dotenv/config';
-import allureWriter from '@shelex/cypress-allure-plugin/writer';
+import { defineConfig } from 'cypress'
+import dotenv from 'dotenv'
+dotenv.config()
+import fs from 'fs'
+import path from 'path'
+import allureWriter from '@shelex/cypress-allure-plugin/writer'
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      allureWriter(on, config);
-      return config;
-    },
-    baseUrl: process.env.BASE_URL,
-    specPattern: 'cypress/e2e/**/*.cy.ts',
-    supportFile: 'cypress/support/e2e.ts'
-  }
-});
+    e2e: {
+        setupNodeEvents(on, config) {
+            // Allure
+            allureWriter(on, config)
+
+            //Task
+            on('task', {
+                getFilesFromFolder(folderPath: string) {
+                    const files = fs.readdirSync(folderPath)
+
+                    return files.map((file) =>
+                        path.join(folderPath, file)
+                    )
+                },
+            })
+
+            return config
+        },
+
+        baseUrl: process.env.BASE_URL,
+        specPattern: 'cypress/e2e/**/*.cy.ts',
+        supportFile: 'cypress/support/e2e.ts',
+        viewportWidth: 1280,
+        viewportHeight: 800,
+    }
+})
