89d3488f4a8722ffbe0964937cef4df70a01425b
VigeteX <73284922+VigeteX@users.noreply.github.com>
2026-05-22 21:49:28 +0300

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,35 @@
 declare const process: any;
-import { defineConfig } from 'cypress';
-import 'dotenv/config';
-import allureWriter from '@shelex/cypress-allure-plugin/writer';
+import { defineConfig } from 'cypress'
+import dotenv from 'dotenv'
+dotenv.config()
+import fs from 'fs'
+import path from 'path'
+import allureWriter from '@shelex/cypress-allure-plugin/writer'
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      allureWriter(on, config);
-      return config;
-    },
-    defaultCommandTimeout: 50000,
-    baseUrl: process.env.BASE_URL,
-    specPattern: 'cypress/e2e/**/*.cy.ts',
-    supportFile: 'cypress/support/e2e.ts'
-  }
-});
+    e2e: {
+        setupNodeEvents(on, config) {
+            // Allure
+            allureWriter(on, config)
+
+            //Task
+            on('task', {
+                getFilesFromFolder(folderPath: string) {
+                    const files = fs.readdirSync(folderPath)
+
+                    return files.map((file) =>
+                        path.join(folderPath, file)
+                    )
+                },
+            })
+
+            return config
+        },
+        defaultCommandTimeout: 50000,
+        baseUrl: process.env.BASE_URL,
+        specPattern: 'cypress/e2e/**/*.cy.ts',
+        supportFile: 'cypress/support/e2e.ts',
+        viewportWidth: 1280,
+        viewportHeight: 800,
+    }
+})
